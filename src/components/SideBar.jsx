3c61a115c0317d8b6915cc511b21afa9d5cbcd0c
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-11 11:51:08 +0000

    style sidebar nav links

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,73 +1,69 @@
 import {
   CDBSidebar,
   CDBSidebarContent,
-  CDBSidebarFooter,
   CDBSidebarHeader,
   CDBSidebarMenu,
   CDBSidebarMenuItem,
 } from "cdbreact";
 import { NavLink } from "react-router-dom";
-import styles from "../styles/SideBar.module.css"
+import styles from "../styles/SideBar.module.css";
 
-import React from 'react'
+import React from "react";
 
 const SideBar = () => {
   // Code for sidebar adapted from:
   // https://www.devwares.com/blog/create-responsive-sidebar-in-react/
   return (
     <div className={styles.Div}>
-      <CDBSidebar className={styles.SideBar}>
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-          <a
-            href="/"
-            className={styles.SideBarHeader}
-          >
+      <CDBSidebar>
+        <CDBSidebarHeader
+          prefix={<i className="fa fa-bars fa-large"></i>}
+          className={styles.Sidebar}
+        >
+          <a href="/" className={styles.SidebarHeaderLink}>
             Syncora
           </a>
         </CDBSidebarHeader>
 
-        <CDBSidebarContent className="sidebar-content">
+        <CDBSidebarContent className={styles.Sidebar}>
           <CDBSidebarMenu>
             <NavLink exact to="/" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/tables" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/profile" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
-            </NavLink>
-            <NavLink exact to="/analytics" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">
-                Analytics
+              <CDBSidebarMenuItem
+                icon="columns"
+                className={styles.SidebarMenuItem}
+              >
+                Dashboard
               </CDBSidebarMenuItem>
             </NavLink>
-
-            <NavLink
-              exact
-              to="/hero404"
-              target="_blank"
-              activeClassName="activeClicked"
-            >
-              <CDBSidebarMenuItem icon="exclamation-circle">
-                404 page
+            <NavLink exact to="/tables" activeClassName="activeClicked">
+              <CDBSidebarMenuItem
+                icon="table"
+                className={styles.SidebarMenuItem}
+              >
+                Tables
+              </CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/profile" activeClassName="activeClicked">
+              <CDBSidebarMenuItem
+                icon="user"
+                className={styles.SidebarMenuItem}
+              >
+                Profile page
+              </CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/analytics" activeClassName="activeClicked">
+              <CDBSidebarMenuItem
+                icon="chart-line"
+                className={styles.SidebarMenuItem}
+              >
+                Analytics
               </CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
-
-        <CDBSidebarFooter style={{ textAlign: "center" }}>
-          <div
-            style={{
-              padding: "20px 5px",
-            }}
-          >
-            Sidebar Footer
-          </div>
-        </CDBSidebarFooter>
       </CDBSidebar>
     </div>
   );
-}
+};
 
-export default SideBar
+export default SideBar;

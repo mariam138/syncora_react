0dc6e74bb166a5a3d2a2f136cf933ea60a8a3d29
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-11 10:45:23 +0000

    feat: create error page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useRouteError } from 'react-router-dom'
+
+function ErrorPage() {
+
+    const error = useRouteError();
+    console.log(error)
+  return (
+      <div>
+          <h1>Error Page</h1>
+          <p>{error.statusText || error.message}</p>
+    </div>
+  )
+}
+
+export default ErrorPage

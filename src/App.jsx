9b4cabfe9897057fb430d9342b3be11397b87c91
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-11 11:09:44 +0000

    import app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import styles from './App.module.css'
 import Root from './routes/root'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className={styles.App}>
         <Routes>
           <Route index element={<Root/>}  />
         </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorPage from './components/ErrorPage.jsx'
 

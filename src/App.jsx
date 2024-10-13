d71a0e4cfa5362e894ea9854151732fab15ffe14
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-13 11:40:33 +0000

    import spinner component for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import styles from './App.module.css'
 import Root from './routes/root'
+import LoadingSpinner from './components/LoadingSpinner'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route index element={<Root/>}  />
         </Routes>
+        <LoadingSpinner />
       </div>
     </>
   )

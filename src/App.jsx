c941ff5207411d496b803f6618ad8a8342b9a235
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-13 11:42:30 +0000

    remove spinner component from app after testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import styles from './App.module.css'
 import Root from './routes/root'
-import LoadingSpinner from './components/LoadingSpinner'
 
 function App() {
 
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route index element={<Root/>}  />
         </Routes>
-        <LoadingSpinner />
       </div>
     </>
   )

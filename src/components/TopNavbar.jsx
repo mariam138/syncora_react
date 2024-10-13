31008447f7c09cb9054170784d2a77fc68fd8a80
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-13 12:11:28 +0000

    update: change links to react router NavLink components

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom';
 
 function TopNavbar() {
   return (
@@ -7,15 +8,15 @@ function TopNavbar() {
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Nav className="me-auto">
-            <Nav.Link>
+            <NavLink to="/events/new">
               New Event <i class="fa-regular fa-calendar-plus"></i>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/tasks/new">
               New Task <i class="fa-solid fa-file-circle-plus"></i>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/notes/new">
               New Note <i class="fa-solid fa-notes-medical"></i>
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Container>
       </Navbar>

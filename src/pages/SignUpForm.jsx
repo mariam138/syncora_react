0a52bee6ea1e9e0a84192b71ae0fa930782e7b86
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-13 13:37:57 +0000

    feat: start creation of sign up form component

---
+++ src/pages/SignUpForm.jsx
@@ -0,0 +1,12 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+import React from 'react'
+
+function SignUpForm() {
+  return (
+    <div>SignUpForm</div>
+  )
+}
+
+export default SignUpForm

bc97265dd60377e84b2cd986455ba98d1bebcc51
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-13 10:00:19 +0000

    refactor: create separate sidebar menu links component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,6 +10,110 @@ import styles from "../styles/SideBar.module.css";
 
 import React, { useEffect, useState } from "react";
 
+const SidebarMenuLinks = ({ screenSize }) => {
+  return (
+    <>
+      {screenSize && (
+        <>
+          {/* Quick create links for mobile */}
+          <NavLink exact to="/events/new" activeClassName="activeClicked">
+            <CDBSidebarMenuItem
+              icon="calendar-plus"
+              iconSize="lg"
+              className={styles.SidebarMenuItem}
+            >
+              New Event
+            </CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/tasks/new" activeClassName="activeClicked">
+            <CDBSidebarMenuItem
+              icon="file-circle-plus"
+              iconSize="lg"
+              className={styles.SidebarMenuItem}
+            >
+              New Task
+            </CDBSidebarMenuItem>
+          </NavLink>
+          <NavLink exact to="/notes/new" activeClassName="activeClicked">
+            <CDBSidebarMenuItem
+              icon="square-plus"
+              iconSize="lg"
+              className={styles.SidebarMenuItem}
+            >
+              New Note
+            </CDBSidebarMenuItem>
+          </NavLink>
+          <hr />
+        </>
+      )}
+      <NavLink exact to="/" activeClassName="activeClicked">
+        <CDBSidebarMenuItem
+          icon="table-columns"
+          iconSize="lg"
+          className={styles.SidebarMenuItem}
+        >
+          Dashboard
+        </CDBSidebarMenuItem>
+      </NavLink>
+
+      {/* Events page link */}
+      <NavLink exact to="/events" activeClassName="activeClicked">
+        <CDBSidebarMenuItem
+          icon="calendar-day"
+          iconSize="lg"
+          className={styles.SidebarMenuItem}
+        >
+          Events
+        </CDBSidebarMenuItem>
+      </NavLink>
+
+      {/* Tasks page link */}
+      <NavLink exact to="/tasks" activeClassName="activeClicked">
+        <CDBSidebarMenuItem
+          icon="list"
+          iconSize="lg"
+          className={styles.SidebarMenuItem}
+        >
+          Tasks
+        </CDBSidebarMenuItem>
+      </NavLink>
+
+      {/* Notes page link */}
+      <NavLink exact to="/notes" activeClassName="activeClicked">
+        <CDBSidebarMenuItem
+          className={styles.SidebarMenuItem}
+          icon="sticky-note"
+          iconSize="lg"
+        >
+          Notes
+        </CDBSidebarMenuItem>
+      </NavLink>
+
+      {/* Profile page link */}
+      <NavLink exact to="" activeClassName="activeClicked">
+        <CDBSidebarMenuItem
+          className={styles.SidebarMenuItem}
+          icon="user"
+          iconSize="lg"
+        >
+          Profile
+        </CDBSidebarMenuItem>
+      </NavLink>
+
+      {/* Sign out link */}
+      <NavLink exact to="" activeClassName="activeClicked">
+        <CDBSidebarMenuItem
+          className={styles.SidebarMenuItem}
+          icon="right-from-bracket"
+          iconSize="lg"
+        >
+          Sign Out
+        </CDBSidebarMenuItem>
+      </NavLink>
+    </>
+  );
+};
+
 const SideBar = () => {
   // Code to conditionally render the quick links in the navbar
   // Based on the screen size is adapted from this post:
@@ -24,7 +128,7 @@ const SideBar = () => {
   useEffect(() => {
     window.addEventListener("resize", updateSidebarMenu);
     return () => window.removeEventListener("resize", updateSidebarMenu);
-  });
+  }, []);
 
   // Code for sidebar adapted from:
   // https://www.devwares.com/blog/create-responsive-sidebar-in-react/
@@ -44,174 +148,9 @@ const SideBar = () => {
 
         <CDBSidebarContent className={styles.Sidebar}>
           <CDBSidebarMenu>
-            {screenSize ? (
-              <>
-                {/* Quick create links for mobile */}
-                <NavLink exact to="/events/new" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    icon="calendar-plus"
-                    iconSize="lg"
-                    className={styles.SidebarMenuItem}
-                  >
-                    New Event
-                  </CDBSidebarMenuItem>
-                </NavLink>
-                <NavLink exact to="/tasks/new" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    icon="file-circle-plus"
-                    iconSize="lg"
-                    className={styles.SidebarMenuItem}
-                  >
-                    New Task
-                  </CDBSidebarMenuItem>
-                </NavLink>
-                <NavLink exact to="/notes/new" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    icon="square-plus"
-                    iconSize="lg"
-                    className={styles.SidebarMenuItem}
-                  >
-                    New Note
-                  </CDBSidebarMenuItem>
-                </NavLink>
-                <hr />
-                <NavLink exact to="/" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    icon="table-columns"
-                    iconSize="lg"
-                    className={styles.SidebarMenuItem}
-                  >
-                    Dashboard
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {/* Events page link */}
-                <NavLink exact to="/events" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    icon="calendar-day"
-                    iconSize="lg"
-                    className={styles.SidebarMenuItem}
-                  >
-                    Events
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {/* Tasks page link */}
-                <NavLink exact to="/tasks" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    icon="list"
-                    iconSize="lg"
-                    className={styles.SidebarMenuItem}
-                  >
-                    Tasks
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {/* Notes page link */}
-                <NavLink exact to="/notes" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    className={styles.SidebarMenuItem}
-                    icon="sticky-note"
-                    iconSize="lg"
-                  >
-                    Notes
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {/* Profile page link */}
-                <NavLink exact to="" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    className={styles.SidebarMenuItem}
-                    icon="user"
-                    iconSize="lg"
-                  >
-                    Profile
-                  </CDBSidebarMenuItem>
-                </NavLink>
-
-                {/* Sign out link */}
-                <NavLink exact to="" activeClassName="activeClicked">
-                  <CDBSidebarMenuItem
-                    className={styles.SidebarMenuItem}
-                    icon="right-from-bracket"
-                    iconSize="lg"
-                  >
-                    Sign Out
-                  </CDBSidebarMenuItem>
-                </NavLink>
-              </>
-            ) : (
-              <>
-                {/* Dashboard link */}
-                <CDBSidebarMenu>
-                  <NavLink exact to="/" activeClassName="activeClicked">
-                    <CDBSidebarMenuItem
-                      icon="table-columns"
-                      iconSize="lg"
-                      className={styles.SidebarMenuItem}
-                    >
-                      Dashboard
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-
-                  {/* Events page link */}
-                  <NavLink exact to="/events" activeClassName="activeClicked">
-                    <CDBSidebarMenuItem
-                      icon="calendar-day"
-                      iconSize="lg"
-                      className={styles.SidebarMenuItem}
-                    >
-                      Events
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-
-                  {/* Tasks page link */}
-                  <NavLink exact to="/tasks" activeClassName="activeClicked">
-                    <CDBSidebarMenuItem
-                      icon="list"
-                      iconSize="lg"
-                      className={styles.SidebarMenuItem}
-                    >
-                      Tasks
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-
-                  {/* Notes page link */}
-                  <NavLink exact to="/notes" activeClassName="activeClicked">
-                    <CDBSidebarMenuItem
-                      className={styles.SidebarMenuItem}
-                      icon="sticky-note"
-                      iconSize="lg"
-                    >
-                      Notes
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-
-                  {/* Profile page link */}
-                  <NavLink exact to="" activeClassName="activeClicked">
-                    <CDBSidebarMenuItem
-                      className={styles.SidebarMenuItem}
-                      icon="user"
-                      iconSize="lg"
-                    >
-                      Profile
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-
-                  {/* Sign out link */}
-                  <NavLink exact to="" activeClassName="activeClicked">
-                    <CDBSidebarMenuItem
-                      className={styles.SidebarMenuItem}
-                      icon="right-from-bracket"
-                      iconSize="lg"
-                    >
-                      Sign Out
-                    </CDBSidebarMenuItem>
-                  </NavLink>
-                </CDBSidebarMenu>
-              </>
-            )}
+            <SidebarMenuLinks screenSize={screenSize}/>
           </CDBSidebarMenu>
+
           {/* Separate menu for sign in and register links */}
           <CDBSidebarMenu>
             {/* Sign in link */}

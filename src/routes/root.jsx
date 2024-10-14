5c01f2147676cf7d066bcea3b67d9a583d669d7e
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-14 09:23:51 +0000

    fix: change to padding-left + apply to outlet div

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,13 +3,14 @@ import SignUpForm from "../pages/SignUpForm";
 import SideBar from "../components/SideBar";
 import { Stack } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import appStyles from "../App.module.css";
 
 const root = () => {
   return (
     <>
       <Stack direction="horizontal">
         <SideBar />
-        <div className="mx-auto">
+        <div className={`mx-auto ${appStyles.OutletDiv}`}>
           <Outlet />
         </div>
       </Stack>

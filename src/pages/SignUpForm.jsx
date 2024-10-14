f7a733d26c1e8d7c06ae2ba8cbbc7618d423c8b3
Mariam Ayoub <mar.ayoub98@gmail.com>
2024-10-14 07:23:01 +0000

    move sign up form into card

--- src/pages/SignUpForm.jsx
+++ src/pages/SignUpForm.jsx
@@ -1,31 +1,38 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Card } from "react-bootstrap";
 
-import React from 'react'
+import React from "react";
 
 function SignUpForm() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <div>
+      <h1>Syncora</h1>
+      <h2>Let's get organised.</h2>
+      <Card>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card>
+    </div>
   );
 }
 
-export default SignUpForm
+export default SignUpForm;

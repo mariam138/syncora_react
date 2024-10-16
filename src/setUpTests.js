091d70ee8acfc17dce1ef178de1807b958ffe004
Mariam <mar.ayoub98@gmail.com>
2024-10-16 17:23:39 +0400

    install jest dom testing library

--- src/setUpTests.js
+++ src/setUpTests.js
@@ -1,7 +1,7 @@
 // Testing set up to be used with Jest
 // Code adapted from the msw documentation
 // https://mswjs.io/docs/api/setup-server/
-
+import "@testing-library/jest-dom";
 import setupServer from "msw/node";
 import { handlers } from "./mocks/handlers";
 

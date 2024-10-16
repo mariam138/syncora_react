06693c493ef2245c025bbfea17b2444ebce3c9cc
Mariam <mar.ayoub98@gmail.com>
2024-10-16 16:57:41 +0400

    create base url for handlers

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,12 @@
 import { http, HttpResponse } from "msw";
 
+const baseURL = "https://syncora-api-ecc74194384c.herokuapp.com/ ";
+
 // Check status code when a new user registers to the website
 export const handlers = [
-    http.post('/dj-rest-auth/registration/', async ({ request }) => {
-        const newUser = await request.json()
+  http.post(`${baseURL}dj-rest-auth/registration/`, async ({ request }) => {
+    const newUser = await request.json();
 
-        return HttpResponse.json(newUser, {status: 201})
-    })
-]
+    return HttpResponse.json(newUser, { status: 200 });
+  }),
+];

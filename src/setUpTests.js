d7a4ab7ae417223cc9242294ece31078777f93c6
Mariam <mar.ayoub98@gmail.com>
2024-10-16 17:07:21 +0400

    test: set up msw server for testing

---
+++ src/setUpTests.js
@@ -0,0 +1,20 @@
+// Testing set up to be used with Jest
+// Code adapted from the msw documentation
+// https://mswjs.io/docs/api/setup-server/
+
+import setupServer from "msw/node";
+import { handlers } from "./mocks/handlers";
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});

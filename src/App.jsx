fa608a135e455b3ea87a1e40c9fdfb96ea5e464e
Mariam <mar.ayoub98@gmail.com>
2024-10-17 12:50:13 +0400

    create handleMount for setting current user data

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import Root from "./routes/root";
 import "./api/axiosDefaults";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import api from "./api/axiosDefaults";
 
 function App() {
   // This will be used to check whether a use is logged in or not
   // Initially set to null
   const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await api.get("dj-rest-auth/user");
+      setCurrentUser(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Set the current user data when a component mounts
+  // Empty array to allow the effect to run only once
+  useEffect(() => {
+    handleMount();
+  }, [])
+
   return (
     <>
       <div className={styles.App}>

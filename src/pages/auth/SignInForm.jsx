9fa05d453f774d290871c1a7c6570a20a9b9b7b3
Mariam <mar.ayoub98@gmail.com>
2024-10-17 11:57:40 +0400

    feat: create sign in form component

---
+++ src/pages/auth/SignInForm.jsx
@@ -0,0 +1,101 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Alert, Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import appStyles from "../../App.module.css";
+import styles from "../../styles/SignUpForm.module.css";
+import api from "../../api/axiosDefaults";
+
+import React from "react";
+
+const SignInForm = () => {
+  return (
+    <div className="text-center">
+      <h1 className={appStyles.Header}>Syncora</h1>
+      <h2 className={appStyles.Header}>Let's get organised.</h2>
+      <Card>
+        <Card.Body>
+          <Card.Title>Sign up below to start using Syncora</Card.Title>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Choose a username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {error.username?.map((message, i) => (
+              <Alert variant="warning" key={i}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {error.email?.map((message, i) => (
+              <Alert variant="warning" key={i}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group className="mb-3" controlId="formPassword1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+                value={password1}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {error.password1?.map((message, i) => (
+              <Alert variant="warning" key={i}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group className="mb-3" controlId="formPassword2">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password again"
+                name="password2"
+                value={password2}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {error.password2?.map((message, i) => (
+              <Alert variant="warning" key={i}>
+                {message}
+              </Alert>
+            ))}
+
+            <Button type="submit" className={styles.SignupButton}>
+              Sign Up
+            </Button>
+            <Card.Text>
+              Already have an account? Sign in{" "}
+              <Link exact to="/signin">
+                here.
+              </Link>
+            </Card.Text>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInForm;

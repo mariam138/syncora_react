b7dba277c485cf05c0e73416cdacff9f349a2070
Mariam <mar.ayoub98@gmail.com>
2024-10-17 12:43:13 +0400

    create current user state to check for logged in users

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import Root from "./routes/root";
 import "./api/axiosDefaults";
+import { useState } from "react";
 
 function App() {
+  // This will be used to check whether a use is logged in or not
+  // Initially set to null
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <>
       <div className={styles.App}>

a802567a2aa07ffbb8e40207c5cc53837e5afa55
Mariam <mar.ayoub98@gmail.com>
2024-10-17 15:48:53 +0400

    refactor: wrap current user provider around router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import Root from "./routes/root";
-import "./api/axiosDefaults";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import * as ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './components/ErrorPage.jsx'
-import SignUpForm from './pages/auth/SignUpForm.jsx'
-import Root from './routes/root.jsx'
-import SignInForm from './pages/auth/SignInForm.jsx'
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage.jsx";
+import SignUpForm from "./pages/auth/SignUpForm.jsx";
+import Root from "./routes/root.jsx";
+import SignInForm from "./pages/auth/SignInForm.jsx";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,19 +14,21 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: 'signup',
-        element: <SignUpForm />
+        path: "signup",
+        element: <SignUpForm />,
       },
       {
-        path: 'signin',
-        element: <SignInForm />
-      }
+        path: "signin",
+        element: <SignInForm />,
+      },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CurrentUserProvider>
+      <RouterProvider router={router} />
+    </CurrentUserProvider>
   </StrictMode>,
-)
+);

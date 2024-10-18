9e3bf227dacd11ea003aaba11febbd53491f3c46
Mariam <mar.ayoub98@gmail.com>
2024-10-18 20:38:10 +0400

    get list of profiles and print to console

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import api from "../../api/axiosDefaults";
 
 function ProfilePage() {
-  return (
-    <div>ProfilePage</div>
-  )
+  const [profileData, setProfileData] = useState({
+    profile: { results: [] },
+  });
+  const { profile } = profileData;
+
+  const handleMount = async () => {
+    try {
+      const { data } = await api.get("/profiles");
+      console.log(data);
+      setProfileData({
+        profile: data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+  return <div>ProfilePage</div>;
 }
 
-export default ProfilePage
+export default ProfilePage;

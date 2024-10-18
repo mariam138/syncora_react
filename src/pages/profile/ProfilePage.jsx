713ef9a44607e98b598756403f89e766874e03b7
Mariam <mar.ayoub98@gmail.com>
2024-10-18 21:05:23 +0400

    feat: display user's profile image

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import api from "../../api/axiosDefaults";
+import Image from "react-bootstrap/Image";
 
 function ProfilePage() {
   const [profileData, setProfileData] = useState({
@@ -26,10 +27,12 @@ function ProfilePage() {
   useEffect(() => {
     handleMount();
   }, []);
-    const { id, username, name, email } = profile;
+    const { id, username, name, email, profile_image } = profile;
+    // console.log(profile_image)
   return (
     <>
           <h1>{username}'s Profile</h1>
+          <Image src={profile_image} roundedCircle/>
     </>
   );
 }

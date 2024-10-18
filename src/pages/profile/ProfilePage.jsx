7f8b4485af1235adfd66ad4ebc9e13e602391d18
Mariam <mar.ayoub98@gmail.com>
2024-10-18 20:49:29 +0400

    get profile data of current user

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -7,12 +7,13 @@ function ProfilePage() {
     profile: { results: [] },
   });
   const { profile } = profileData;
+  const currentUser = useCurrentUser();
 
   // Get list of profiles from api and log it to the console
   // When component is mounted
   const handleMount = async () => {
     try {
-      const { data } = await api.get("/profiles");
+      const { data } = await api.get(`/profiles/${currentUser.pk}`);
       console.log(data);
       setProfileData({
         profile: data,
@@ -26,7 +27,14 @@ function ProfilePage() {
   useEffect(() => {
     handleMount();
   }, []);
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      {/* {profile.results.map(pfp => (
+                <p key={pfp.id}>{pfp.username}|{pfp.profile_image}</p> */}
+      {/* ))} */}
+      <h1>Profile</h1>
+    </>
+  );
 }
 
 export default ProfilePage;

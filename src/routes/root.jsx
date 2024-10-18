8c37dd24a55edfcb1920f4b1f1b6f5ada1727320
Mariam <mar.ayoub98@gmail.com>
2024-10-18 20:11:06 +0400

    maint: add docstring to root function

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,13 +3,32 @@ import { Stack } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 import TopNavbar from "../components/TopNavbar";
+import { useState, useEffect } from "react";
 
+/**
+ * This defines the root function, where all remaining roots become
+ * children of. The <Outlet /> component provided by
+ * React Router is where all children components are displayed.
+ * The root will constantly show the <Sidebar /> component
+ * and conditionally show the <TopNavbar /> component only if
+ * a user is authenticated and the screen size is larger
+ * than 992 px.
+ */
 const root = () => {
   const currentUser = useCurrentUser();
+  const [screenSize, setScreenSize] = useState(window.innerWidth < 992);
+  const updateTopNavbar = () => {
+    setScreenSize(window.innerWidth < 992);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateTopNavbar);
+    return () => window.removeEventListener("resize", updateTopNavbar);
+  }, []);
   return (
     <>
       <Stack direction="horizontal">
-        {currentUser && <TopNavbar />}
+        {currentUser && !screenSize && <TopNavbar />}
         <SideBar />
         <div className={`mx-auto `}>
           <Outlet />

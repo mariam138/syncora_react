ae20c4dbcae28f3f3eb2f8c045060b924663b5a5
Mariam <mar.ayoub98@gmail.com>
2024-10-18 20:12:46 +0400

    refactor: make dashboard a page instead of a route

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import TopNavbar from "../components/TopNavbar";
+import { Outlet } from "react-router-dom";
+
+const Dashboard = () => {
+
+  return (
+    <div>Dashboard</div>
+  );
+};
+
+export default Dashboard;

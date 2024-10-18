ea30ec4fd8993b8d98271a3dae6839a2ae199784
Mariam <mar.ayoub98@gmail.com>
2024-10-18 12:48:42 +0400

    create state for screensize

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopNavbar from "../components/TopNavbar";
 
 const Dashboard = () => {
+
+  const [screenSize, setScreenSize] = useState(window.innerWidth < 992);
+  const updateTopNavbar = () => {
+    setScreenSize(window.innerWidth < 992);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateTopNavbar);
+    return () => window.removeEventListener("resize", updateTopNavbar);
+  }, [])
+
   return (
     <>
       <TopNavbar />

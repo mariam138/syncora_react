cb44d56c8dc42fc85c5ce5d9ecb38645afae23fb
Mariam <mar.ayoub98@gmail.com>
2024-10-18 12:25:16 +0400

    feat: create dashboard route

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopNavbar from '../components/TopNavbar'
+
+const dashboard = () => {
+  return (
+      <>
+        <TopNavbar/>
+      </>
+  )
+}
+
+export default dashboard

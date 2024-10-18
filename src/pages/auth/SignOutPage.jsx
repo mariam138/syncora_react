907cb51cf94414ec9011b6dabd167c52bcb91d61
Mariam <mar.ayoub98@gmail.com>
2024-10-18 13:28:50 +0400

    feat: add go back button to sign out page

--- src/pages/auth/SignOutPage.jsx
+++ src/pages/auth/SignOutPage.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function SignOutPage() {
+  // Use react-router's useNavigate hook to allow the user to go back
+  // to the previous page if they change their mind about logging out
+  // Adapted from the docs:
+  // https://reactrouter.com/en/main/start/tutorial#cancel-button
+  const navigate = useNavigate();
   return (
     <>
       <Container fluid="lg">
         <Row>
           <Col>
             <h1>Are you sure you want to sign out?</h1>
+            <Button
+              type="button"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              Go back
+            </Button>
+            <Button>Sign Out</Button>
           </Col>
         </Row>
       </Container>

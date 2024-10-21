29fca97e131fd80fac37d7e9e33b8b8383bb4543
Mariam <mar.ayoub98@gmail.com>
2024-10-21 16:57:11 +0400

    style sign in form with hero img

--- src/pages/auth/SignInForm.jsx
+++ src/pages/auth/SignInForm.jsx
@@ -1,12 +1,12 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Alert, Card } from "react-bootstrap";
+import { Alert, Card, Col, Row } from "react-bootstrap";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import appStyles from "../../App.module.css";
 import api from "../../api/axiosDefaults";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
-
+import Image from "react-bootstrap/Image";
 
 const SignInForm = () => {
   const setCurrentUser = useSetCurrentUser();
@@ -41,58 +41,75 @@ const SignInForm = () => {
   };
 
   return (
-    <div className="text-center px-3">
-      <h1 className={appStyles.Header}>Syncora</h1>
-      <h2 className={appStyles.Header}>Let's get organised.</h2>
-      <Card>
-        <Card.Body>
-          <Card.Title>Sign in below to Syncora</Card.Title>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {error.username?.map((message, i) => (
-              <Alert variant="warning" key={i}>
-                {message}
-              </Alert>
-            ))}
+    <Row>
+      <div className="text-center px-3">
+        <h1 className={appStyles.Header}>Syncora</h1>
+        <h2 className={appStyles.Header}>Let's get organised.</h2>
+      </div>
+      <Col md={6} className="gx-0">
+        <Card>
+          <Card.Body>
+            <Card.Title className="text-center">
+              Sign in below to Syncora
+            </Card.Title>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {error.username?.map((message, i) => (
+                <Alert variant="warning" key={i}>
+                  {message}
+                </Alert>
+              ))}
 
-            <Form.Group className="mb-3" controlId="formPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {error.password?.map((message, i) => (
-              <Alert variant="warning" key={i}>
-                {message}
-              </Alert>
-            ))}
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+              {error.password?.map((message, i) => (
+                <Alert variant="warning" key={i}>
+                  {message}
+                </Alert>
+              ))}
+              <div
+                className="text-center
+              "
+              >
+                <Button type="submit" className={appStyles.Button}>
+                  Sign In
+                </Button>
+              </div>
+              <Card.Text className="text-center">
+                Don't have an account? Sign up{" "}
+                <Link exact to="/signup">
+                  here.
+                </Link>
+              </Card.Text>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Col>
 
-            <Button type="submit" className={appStyles.Button}>
-              Sign In
-            </Button>
-            <Card.Text>
-              Don't have an account? Sign up{" "}
-              <Link exact to="/signup">
-                here.
-              </Link>
-            </Card.Text>
-          </Form>
-        </Card.Body>
-      </Card>
-    </div>
+      <Col md={6} className="gx-0">
+        <Image
+          className={`d-none d-md-block ${appStyles.Hero}`}
+          src="https://res.cloudinary.com/dy1xfelbe/image/upload/v1729512622/lists-6131220_1280_xvupzh.jpg"
+        />
+      </Col>
+    </Row>
   );
 };
 

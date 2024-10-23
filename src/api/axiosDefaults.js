b93a0a11b4b1589e5f390cc6d7abe96ffff0dfb0
Mariam <mar.ayoub98@gmail.com>
2024-10-23 11:44:12 +0400

    maint: remove commented out code

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,13 +2,8 @@ import axios from "axios";
 
 // Create axios instance and export it for use in other components
 // Code adapted from: https://axios-http.com/docs/config_defaults
-// const api = axios.create({
-//   baseURL: "https://syncora-api-ecc74194384c.herokuapp.com/",
-// });
 // // Allow images to also be uploaded when changing profile pic
-// api.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // // Avoid CORS errors while sending cookies
-// api.defaults.withCredentials = true;
 const api = axios.create({
   baseURL: "https://syncora-api-ecc74194384c.herokuapp.com/",
   headers: { post: { "Content-Type": "multipart/form-data" } },
@@ -17,10 +12,3 @@ const api = axios.create({
 });
 
 export default api;
-
-// axios.defaults.baseURL = "https://syncora-api-ecc74194384c.herokuapp.com/";
-// axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-// axios.defaults.withCredentials = true;
-
-// export const axiosReq = axios.create();
-// export const axiosRes = axios.create();

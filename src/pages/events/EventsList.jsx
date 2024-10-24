034b706a19cb1be657b591b68224b32f4527e861
Mariam <mar.ayoub98@gmail.com>
2024-10-24 18:32:30 +0400

    add constructor to events list class component

--- src/pages/events/EventsList.jsx
+++ src/pages/events/EventsList.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export class EventsList extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         first
+      }
+    }
   render() {
     return (
       <div>EventsList</div>

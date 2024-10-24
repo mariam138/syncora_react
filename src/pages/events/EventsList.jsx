1244501a921d61b9d90fc8cf3ea075cd53d1d038
Mariam <mar.ayoub98@gmail.com>
2024-10-24 18:12:07 +0400

    feat: create events list page component

---
+++ src/pages/events/EventsList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class EventsList extends Component {
+  render() {
+    return (
+      <div>EventsList</div>
+    )
+  }
+}
+
+export default EventsList

379bdfac7040a46d292b952d3c116bae139e4d60
Mariam <mar.ayoub98@gmail.com>
2024-10-25 15:49:46 +0400

    add bootstrap grid styling

--- src/pages/events/EventsList.jsx
+++ src/pages/events/EventsList.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { apiResp } from "../../api/axiosDefaults";
 import LoadingSpinner from "../../components/LoadingSpinner";
-import appStyles from '../../App.module.css';
+import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function EventsList() {
   // Set events list to an empty results array
   const [eventsList, setEventsList] = useState({ results: [] });
   // Initially set loaded state to false
   const [isLoaded, setIsLoaded] = useState(false);
+  const currentUser = useCurrentUser();
 
   const handleMount = async () => {
     try {
@@ -21,17 +25,23 @@ function EventsList() {
 
   useEffect(() => {
     handleMount();
-  }, []);
+  }, [currentUser]);
 
   return (
-    <div>
-      <h1 className={appStyles.Header}>Events</h1>
-      {isLoaded ? (
-        eventsList.results.map((event) => <p key={event.id}>{event.name}</p>)
-      ) : (
-        <LoadingSpinner />
-      )}
-    </div>
+    <>
+      <Row>
+        <Col sm={{ span: 6, offset: 3 }}>
+          <h1 className={appStyles.Header}>Events</h1>
+          {isLoaded ? (
+            eventsList.results.map((event) => (
+              <p key={event.id}>{event.name}</p>
+            ))
+          ) : (
+            <LoadingSpinner />
+          )}
+        </Col>
+      </Row>
+    </>
   );
 }
 

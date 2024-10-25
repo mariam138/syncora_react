9a3c5ad5798e067dc41292ede2f8630fc693dafb
Mariam <mar.ayoub98@gmail.com>
2024-10-25 15:26:13 +0400

    fetch list of events on mount

--- src/pages/events/EventsList.jsx
+++ src/pages/events/EventsList.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import { apiResp } from "../../api/axiosDefaults";
 
 function EventsList() {
   // Set events list to an empty results array
   const [eventsList, setEventsList] = useState({ results: [] });
   // Initially set loaded state to false
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await apiResp.get("/events");
+      setEventsList(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <div>EventsList</div>;
 }
 

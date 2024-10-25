2bf858e651eb24fed1879c4060b1347c48af3382
Mariam <mar.ayoub98@gmail.com>
2024-10-25 16:13:06 +0400

    push content to top

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -48,7 +48,7 @@ const root = () => {
           </Navbar>
         )}
 
-        <Container className={appStyles.Container}>
+        <Container className={`${appStyles.Container} mt-5 mb-auto`}>
               <Outlet />
         </Container>
       </Stack>

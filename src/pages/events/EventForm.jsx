4871581bc10112edd8d49e73c242ffd7f3ac43d9
Mariam <mar.ayoub98@gmail.com>
2024-10-28 16:47:28 +0400

    start form creation + install react date picker

--- src/pages/events/EventForm.jsx
+++ src/pages/events/EventForm.jsx
@@ -9,7 +9,21 @@ import appStyles from "../../App.module.css";
 function EventForm() {
   return (
     <>
-      <h1 className={appStyles.Header}>New Event</h1>
+      <Row>
+        <Col sm={{ span: 6, offset: 3 }}>
+          <h1 className={appStyles.Header}>New Event</h1>
+          <Card>
+            <Card.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="email" placeholder="Event name" />
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </>
   );
 }

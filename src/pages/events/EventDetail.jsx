01e55105bafed8574be14e4f2b6a95bc1ed4cd78
Mariam <mar.ayoub98@gmail.com>
2024-10-28 12:34:13 +0400

    fetch event detail

--- src/pages/events/EventDetail.jsx
+++ src/pages/events/EventDetail.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { apiResp } from '../../api/axiosDefaults';
 
 function EventDetail() {
+    const id = useParams();
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    const handleMount = async () => {
+        try {
+            const {data} = await apiResp.get(`/events/${id}`)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        handleMount();
+    }, [id])
+
   return (
     <div>EventDetail</div>
   )

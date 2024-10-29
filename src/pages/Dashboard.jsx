3dbf722778542fbc711c373e2301d17544603cb1
Mariam <mar.ayoub98@gmail.com>
2024-10-29 19:00:30 +0400

    style dashboard with bootstrap row and col

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import EventsList from "./events/EventsList";
 import appStyles from "../App.module.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Dashboard() {
   return (
     <>
-      <h1 className={appStyles.Header}>Dashboard</h1>
+      <Row>
+        <h1 className={appStyles.Header}>Dashboard</h1>
+      </Row>
+
+      {/* <Row> */}
+      {/* <Col> */}
       <h2 className={appStyles.Header}>Events</h2>
       <EventsList
         showHeader={false}
         showCreateLink={false}
         showDeleteButton={false}
       />
+      {/* </Col> */}
+      {/* </Row> */}
     </>
   );
 }

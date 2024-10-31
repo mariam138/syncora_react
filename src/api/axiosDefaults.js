e3e718ad25ad8346b4faa557c6aa88204695eb1f
Mariam <mar.ayoub98@gmail.com>
2024-10-31 13:30:50 +0400

    maint: tidy up comments

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 // Create axios instance and export it for use in other components
 // Code adapted from: https://axios-http.com/docs/config_defaults
-// // Allow images to also be uploaded when changing profile pic
-// // Avoid CORS errors while sending cookies
+// Allow images to also be uploaded when changing profile pic
+// Avoid CORS errors while sending cookies
 const api = axios.create({
   baseURL: "https://syncora-api-ecc74194384c.herokuapp.com/",
   headers: { post: { "Content-Type": "multipart/form-data" } },
-
   withCredentials: true,
 });
 

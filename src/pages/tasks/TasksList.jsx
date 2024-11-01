d53f75190cc83a87e7ee78603ac421c3cb4e7a09
Mariam <mar.ayoub98@gmail.com>
2024-11-01 13:44:37 +0400

    create tabbed card component for tasks

--- src/pages/tasks/TasksList.jsx
+++ src/pages/tasks/TasksList.jsx
@@ -5,6 +5,10 @@ import appStyles from "../../App.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useNavigate } from "react-router-dom";
 import { apiReq } from "../../api/axiosDefaults";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+import TabContainer from "react-bootstrap/TabContainer";
+import { Tab } from "react-bootstrap";
 
 function TasksList() {
   const [tasksList, setTasksList] = useState({ results: [] });
@@ -17,7 +21,6 @@ function TasksList() {
       const { data } = await apiReq.get("/tasks/");
       setTasksList(data);
       setIsLoaded(true);
-      console.log(data?.results);
     } catch (error) {
       console.log(error);
     }
@@ -29,11 +32,31 @@ function TasksList() {
 
   return (
     <>
-      <Row>
-        <Col sm={{ span: 6, offset: 3 }}>
-          <h1 className={appStyles.Header}>Tasks</h1>
-        </Col>
-      </Row>
+      <Tab.Container id="tasks-tabs" defaultActiveKey="uncompleted">
+        <Row>
+          <Col sm={{ span: 6, offset: 3 }}>
+            <h1 className={appStyles.Header}>Tasks</h1>
+            <Card>
+              <Card.Header>
+                <Nav variant="tabs">
+                  <Nav.Item>
+                    <Nav.Link eventKey="uncompleted">Uncompleted</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="completed">Completed</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Card.Header>
+              <Card.Body>
+                <Tab.Content>
+                  <Tab.Pane eventKey="uncompleted">Uncomplete</Tab.Pane>
+                  <Tab.Pane eventKey="completed">Complete</Tab.Pane>
+                </Tab.Content>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Tab.Container>
     </>
   );
 }

de8716261ff25f5b7d3c6cadda3178d7236efaa2
Mariam <mar.ayoub98@gmail.com>
2024-11-01 13:11:04 +0400

    fetch tasks list data

--- src/pages/tasks/TasksList.jsx
+++ src/pages/tasks/TasksList.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import appStyles from "../../App.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useNavigate } from "react-router-dom";
+import { apiReq } from "../../api/axiosDefaults";
 
 function TasksList() {
   const [tasksList, setTasksList] = useState({ results: [] });
   const [isLoaded, setIsLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  const navigate = useNavigate();
+
+  const handleMount = async () => {
+    try {
+      const { data } = await apiReq.get("/tasks/");
+      setTasksList(data);
+      setIsLoaded(true);
+      console.log(data?.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, [currentUser]);
+
   return (
     <>
       <Row>

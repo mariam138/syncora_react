e9e1264abdf77b5c5712e301b46d20d3105feace
Mariam <mar.ayoub98@gmail.com>
2024-11-01 13:08:14 +0400

    set initial states

--- src/pages/tasks/TasksList.jsx
+++ src/pages/tasks/TasksList.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import appStyles from "../../App.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function TasksList() {
+  const [tasksList, setTasksList] = useState({ results: [] });
+  const [isLoaded, setIsLoaded] = useState(false);
+  const currentUser = useCurrentUser();
   return (
     <>
       <Row>

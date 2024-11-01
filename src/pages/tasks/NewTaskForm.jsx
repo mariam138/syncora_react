c94e3d57e4306d1d4fc31a6e3972e5553f234de3
Mariam <mar.ayoub98@gmail.com>
2024-11-01 16:04:56 +0400

    maint: update imports

--- src/pages/tasks/NewTaskForm.jsx
+++ src/pages/tasks/NewTaskForm.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import appStyles from "../../App.module.css";
+import { SuccessToast, WarningToast } from "../../functions/toasts";
 
 function NewTaskForm() {
   const navigate = useNavigate();
@@ -13,8 +21,9 @@ function NewTaskForm() {
     description: "",
     completed: false,
   });
-    const { title, due_date, priority, category, description, completed } = taskData;
-  return <div>NewTaskForm</div>;
+  const { title, due_date, priority, category, description, completed } =
+    taskData;
+  return <></>;
 }
 
 export default NewTaskForm;

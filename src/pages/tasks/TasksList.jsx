994624aa30c8f0f088e237c3e8169d41dec922ff
Mariam <mar.ayoub98@gmail.com>
2024-11-01 13:06:01 +0400

    set up basic layout

--- src/pages/tasks/TasksList.jsx
+++ src/pages/tasks/TasksList.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import appStyles from "../../App.module.css";
 
 function TasksList() {
   return (
-    <div>TasksList</div>
-  )
+    <>
+      <Row>
+        <Col sm={{ span: 6, offset: 3 }}>
+          <h1 className={appStyles.Header}>Tasks</h1>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default TasksList
+export default TasksList;

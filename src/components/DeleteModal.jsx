7c0380de70a4f660556e2fdf30eee6eebc6a9d42
Mariam <mar.ayoub98@gmail.com>
2024-11-04 16:27:01 +0400

    create isDeleting prop

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -2,22 +2,33 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import React from "react";
 
-const DeleteModal = ({ show, handleClose, feature, modalContent, handleDelete }) => {
+const DeleteModal = ({
+  show,
+  handleClose,
+  feature,
+  modalContent,
+  handleDelete,
+  isDeleting,
+}) => {
   return (
     <Modal show={show} onHide={handleClose} centered>
       <Modal.Header closeButton>
         <Modal.Title>Delete {feature}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {modalContent}?{" "}
-        <strong>This action is not reversible.</strong>
+        {modalContent}? <strong>This action is not reversible.</strong>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
         </Button>
-        <Button variant="danger" type="submit" onClick={handleDelete}>
-          Yes, delete
+        <Button
+          variant="danger"
+          type="submit"
+          onClick={handleDelete}
+          disabled={isDeleting}
+        >
+          {isDeleting ? "Deleting..." : "Yes, delete"}
         </Button>
       </Modal.Footer>
     </Modal>

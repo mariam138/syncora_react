2514e3b68006035997debad5b2c9116590b4dbbb
Mariam <mar.ayoub98@gmail.com>
2024-11-05 19:06:56 +0400

    display task list on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import EventsList from "./events/EventsList";
 import appStyles from "../App.module.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import TasksList from "./tasks/TasksList";
 
 function Dashboard() {
   return (
@@ -11,16 +12,19 @@ function Dashboard() {
         <h1 className={appStyles.Header}>Dashboard</h1>
       </Row>
 
-      {/* <Row> */}
-      {/* <Col> */}
-      <h2 className={appStyles.Header}>Events</h2>
-      <EventsList
-        showHeader={false}
-        showCreateLink={false}
-        showDeleteButton={false}
-      />
-      {/* </Col> */}
-      {/* </Row> */}
+      <Row>
+        {/* <Col> */}
+        <h2 className={appStyles.Header}>Events</h2>
+        <EventsList
+          showHeader={false}
+          showCreateLink={false}
+          showDeleteButton={false}
+        />
+        {/* </Col> */}
+        {/* <Col> */}
+        <TasksList />
+        {/* </Col> */}
+      </Row>
     </>
   );
 }

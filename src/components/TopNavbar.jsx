e4699f4b4f53d411cd1d7e337a96066982228adf
Mariam <mar.ayoub98@gmail.com>
2024-11-07 16:58:36 +0400

    remove searchbar

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -11,54 +11,27 @@ import styles from "../styles/TopNavbar.module.css";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 function TopNavbar() {
-  const [query, setQuery] = useState("");
   // Gets current user information to display username
   const currentUser = useCurrentUser();
-  const handleSearch = (e) => {
-    e.preventDefault();
-  };
 
   return (
     <>
       <Navbar fixed="top" className={styles.Navbar}>
         <Container fluid>
-          <Nav className="d-flex w-100 align-items-center justify-content-between">
-            <div className="mx-auto">
-              <Form inline onSubmit={handleSearch}>
-                <Row>
-                  <Col xs="auto">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search here..."
-                      className="mr-sm-2"
-                      value={query}
-                      onChange={(e) => setQuery(e.target.value)}
-                    />
-                  </Col>
-                  <Col xs="auto">
-                    <Button type="submit" className={`btn ${styles.SearchBtn}`}>
-                      Search
-                    </Button>
-                  </Col>
-                </Row>
-              </Form>
-            </div>
-
-            <div className="d-flex align-items-center">
-              <NavLink to="/events/new/" className={styles.NavbarLinks}>
-                New Event <i class="fa-regular fa-calendar-plus"></i>
-              </NavLink>
-              <NavLink to="/tasks/new/" className={styles.NavbarLinks}>
-                New Task <i class="fa-solid fa-file-circle-plus"></i>
-              </NavLink>
-              <NavLink to="/notes/new/" className={styles.NavbarLinks}>
-                New Note <i class="fa-solid fa-notes-medical"></i>
-              </NavLink>
-              {/* Welcome text to user when logged in */}
-              <Navbar.Text className="px-2">
-                Welcome, {currentUser.username}
-              </Navbar.Text>
-            </div>
+          <Nav className="d-flex  align-items-center ms-auto">
+            <NavLink to="/events/new/" className={styles.NavbarLinks}>
+              New Event <i class="fa-regular fa-calendar-plus"></i>
+            </NavLink>
+            <NavLink to="/tasks/new/" className={styles.NavbarLinks}>
+              New Task <i class="fa-solid fa-file-circle-plus"></i>
+            </NavLink>
+            <NavLink to="/notes/new/" className={styles.NavbarLinks}>
+              New Note <i class="fa-solid fa-notes-medical"></i>
+            </NavLink>
+            {/* Welcome text to user when logged in */}
+            <Navbar.Text className="px-2">
+              Welcome, {currentUser.username}
+            </Navbar.Text>
           </Nav>
         </Container>
       </Navbar>

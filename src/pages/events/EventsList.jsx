4c29f49f27103447d4d3e46832cfe3d49bb67400
Mariam <mar.ayoub98@gmail.com>
2024-11-07 13:01:01 +0400

    indicate to user if event is today

--- src/pages/events/EventsList.jsx
+++ src/pages/events/EventsList.jsx
@@ -16,7 +16,7 @@ import styles from "../../styles/CreateLink.module.css";
 import accordStyles from "../../styles/Accordion.module.css";
 import DeleteModal from "../../components/DeleteModal";
 import { SuccessToast } from "../../functions/toasts";
-import { formatDate, formatToIso } from "../../functions/dateFormat";
+import { formatDate, formatToIsoDateOnly } from "../../functions/dateFormat";
 
 function EventsList({
   showHeader = true,
@@ -104,7 +104,7 @@ function EventsList({
     ["TRAVEL", "Travel"],
   ];
 
-  const now = formatToIso(new Date());
+  const now = formatToIsoDateOnly(new Date());
 
   return (
     <>
@@ -154,7 +154,10 @@ function EventsList({
                           className={accordStyles.AccordionHeader}
                         >
                           <i class="fa-solid fa-scroll"></i> {event.name}
-                          {/* {console.log(now == event.date)} */}
+                          {/* Inidicate to user if event is today */}
+                          {now === event.date && (
+                            <span className="ms-3 text-success">Today</span>
+                          )}
                         </Accordion.Header>
                         <Accordion.Body>
                           <div className="mb-2">

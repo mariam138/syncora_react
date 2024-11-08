5d573bab70ae2124a4b170d531bb8f0d2993b402
Mariam <mar.ayoub98@gmail.com>
2024-11-08 16:59:39 +0400

    create search bar and onChange handler

--- src/pages/notes/NotesList.jsx
+++ src/pages/notes/NotesList.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-function NotesList() {
+import appStyles from "../../App.module.css";
+
+function NotesList({ showHeader = true, showSearchBar = true }) {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setQuery(searchTerm);
+  };
   return (
-    <div>NotesList</div>
-  )
+    <>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+          {showHeader && <h1 className={appStyles.Header}>Notes</h1>}
+          {showSearchBar && (
+            <div className="d-flex align-items-center mb-2">
+              <Form className="d-flex w-auto ms-auto">
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  value={query}
+                  onChange={handleSearch}
+                />
+                <Button
+                  variant="outline-secondary"
+                  onClick={() => setQuery("")}
+                >
+                  Clear
+                </Button>
+              </Form>
+            </div>
+          )}
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default NotesList
+export default NotesList;

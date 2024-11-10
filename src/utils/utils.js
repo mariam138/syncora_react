d8704c0184caa93acd3955e7f661c9e5cce427e3
Mariam <mar.ayoub98@gmail.com>
2024-11-10 13:37:23 +0400

    create bool var to check token refreshing

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,11 @@
-import { jwtDecode } from "jwt-decode"
+import { jwtDecode } from "jwt-decode";
 
 export const setTokenTimestamp = (data) => {
-    const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp
-    localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp)
-}
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+// Create bool to check if token needs refreshing
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestamp");
+};

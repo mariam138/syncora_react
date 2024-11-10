17697a7afdc99e022953b4615da19e8e2c885d50
Mariam <mar.ayoub98@gmail.com>
2024-11-10 18:14:48 +0400

    create basic note detail layout

--- src/pages/notes/NoteDetail.jsx
+++ src/pages/notes/NoteDetail.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function NoteDetail() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <div>NoteDetail</div>
-  )
+    <>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+          <h1>Note title</h1>
+          <Card>
+            <Card.Body>
+              <Card.Text>Note content blah blah blah</Card.Text>
+              <Card.Text>Date updated</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default NoteDetail
+export default NoteDetail;

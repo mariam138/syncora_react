54ad14fe3e4f4164a3f9b9d9327665a112334c8d
Mariam <mar.ayoub98@gmail.com>
2024-11-10 19:19:02 +0400

    create note form

--- src/pages/notes/NoteForm.jsx
+++ src/pages/notes/NoteForm.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+
+import appStyles from "../../App.module.css";
 
 function NoteForm() {
   return (
-    <div>NoteForm</div>
-  )
+    <>
+      <Row>
+        <Col md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+          <h1>New Note</h1>
+          <Card className="mb-3">
+            <Card.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formTitle">
+                  <Form.Label>
+                    Title <i class="fa-solid fa-heading"></i>
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Task title"
+                    name="title"
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formTitle">
+                  <Form.Label>
+                    Content <i class="fa-solid fa-heading"></i>
+                  </Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows={3}
+                    placeholder="A penny for your thoughts?"
+                    name="content"
+                  />
+                </Form.Group>
+                <div className="text-center">
+                  <Button className={`${appStyles.Button} btn`} type="submit">
+                    Create <i class="fa-solid fa-plus"></i>
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default NoteForm
+export default NoteForm;

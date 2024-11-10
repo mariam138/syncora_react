7a2faa93444598b13707254827cc20403f7b2edb
Mariam <mar.ayoub98@gmail.com>
2024-11-10 18:29:06 +0400

    add background colour to note

--- src/pages/notes/NoteDetail.jsx
+++ src/pages/notes/NoteDetail.jsx
@@ -22,7 +22,7 @@ function NoteDetail() {
       <Row>
         <Col md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
           <h1>Note title</h1>
-          <Card>
+          <Card bg="warning">
             <Card.Body>
               <Card.Text>Note content blah blah blah</Card.Text>
               <Card.Text>Date updated</Card.Text>

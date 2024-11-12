6c2b85710668505a12ee60e1161eb75ac2734b73
Mariam <mar.ayoub98@gmail.com>
2024-11-12 13:04:08 +0400

    create custom styling for loading spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,10 +1,13 @@
 import Spinner from "react-bootstrap/Spinner";
+import styles from '../styles/Spinner.module.css'
 
 function LoadingSpinner() {
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+    <div className="text-center my-2">
+      <Spinner animation="border" role="status" className={`fs-5 ${styles.Spinner}`}>
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
 

250eca00331d41144b18a6d87c8d39a534a697aa
Mariam <mar.ayoub98@gmail.com>
2024-11-12 16:57:22 +0400

    update test import

--- src/pages/__test__/SignUpForm.test.js
+++ src/pages/__test__/SignUpForm.test.js
@@ -1,14 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
-import SignUpForm from "../SignUpForm";
+import SignUpForm from "../auth/SignUpForm";
 
-// describe("Empty registration form", async () => {
-//   render(<SignUpForm />);
-//   const button = await screen.getByText("Sign Up");
-//   fireEvent.click(button);
-//   const formError = screen.getByText("This field may not be blank.");
-//   expect(formError).toBeInTheDocument;
-// });
 
 // TypeError: default is not a function
 describe("Empty registration form", () => {

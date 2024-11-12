288566f5f871c76beaab4f463e1d27c468fc74fc
Mariam <mar.ayoub98@gmail.com>
2024-11-12 21:33:42 +0400

    style error page component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,16 +1,45 @@
 import { useRouteError } from "react-router-dom";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+
 import styles from "../App.module.css";
+import btnStyles from "../styles/DetailPageButtons.module.css";
+import appStyles from "../App.module.css";
+import { useNavigate } from "react-router-dom";
 
 function ErrorPage() {
   // Code to create the error page component is adapted from the react router docs
   // https://reactrouter.com/en/main/start/tutorial#handling-not-found-errors
 
   const error = useRouteError();
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className={styles.App}>
-      <h1>Error Page</h1>
-      <p>{error.statusText || error.message}</p>
-    </div>
+    <Row className="justify-content-center align-items-center">
+      <Col md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }}>
+        <h1 className={`${appStyles.Header} text-center mb-4`}>
+          Oops! There's an error.
+        </h1>
+        <div className={`${styles.App} text-center`}>
+          <p className="fs-5">
+            {error.statusText ||
+              error.message ||
+              "An unexpected error occured."}
+          </p>
+          <Button
+            className={`btn ${appStyles.Button} mx-2 ${btnStyles.BtnText}`}
+            onClick={goBack}
+          >
+            <i className="fa-solid fa-arrow-left"></i> Back
+          </Button>
+        </div>
+      </Col>
+    </Row>
   );
 }
 

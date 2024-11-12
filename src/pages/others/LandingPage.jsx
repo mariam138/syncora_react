df290e059235fbf2739408d4c05105924419d617
Mariam <mar.ayoub98@gmail.com>
2024-11-12 12:24:04 +0400

    create landing page content

--- src/pages/others/LandingPage.jsx
+++ src/pages/others/LandingPage.jsx
@@ -10,7 +10,41 @@ import Image from "react-bootstrap/Image";
 import appStyles from "../../App.module.css";
 
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <Row className="mx-md-4">
+      <div className="text-center px-3">
+        <h1 className={appStyles.Header}>Syncora</h1>
+        <h2 className={appStyles.Header}>Your Digital Organiser</h2>
+      </div>
+
+      <Col md={6} className="gx-md-0">
+        <Card className="my-4">
+          <Card.Body className="text-center">
+            <Card.Title>Organise Your Life with Syncora</Card.Title>
+            <Card.Text>
+              Syncora brings together your tasks, events and notes in one place.
+              It's the perfect digital solution to stay on top of your
+              day-to-day life and be more productive.
+            </Card.Text>
+            <Button className={appStyles.Button} as={Link} to="/signup">
+              Sign Up Now
+            </Button>
+            <Card.Text className="mt-3">
+              Already have an account? <Link to="/signin">Sign in here</Link>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col md={6} className="gx-md-0">
+        <Image
+          className={`d-none d-md-block ${appStyles.Hero}`}
+          src="https://res.cloudinary.com/dy1xfelbe/image/upload/v1729512622/lists-6131220_1280_xvupzh.jpg"
+          alt="Illustration of organized notes and lists"
+        />
+      </Col>
+    </Row>
+  );
 }
 
 export default LandingPage;

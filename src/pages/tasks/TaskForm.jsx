e253a01bfdf944171bcd752fe488f461cf1fec7b
Mariam <mar.ayoub98@gmail.com>
2024-11-12 19:41:52 +0400

    remove unused react var

--- src/pages/tasks/TaskForm.jsx
+++ src/pages/tasks/TaskForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/useCurrentUser";
 import Card from "react-bootstrap/Card";

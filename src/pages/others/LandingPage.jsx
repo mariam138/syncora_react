40e8d5a7b2cfc265da31b7f0b4c3ca247e88311c
Mariam <mar.ayoub98@gmail.com>
2024-11-12 12:07:34 +0400

    maint: add imports

--- src/pages/others/LandingPage.jsx
+++ src/pages/others/LandingPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+import appStyles from "../../App.module.css";
 
 function LandingPage() {
-  return (
-    <div>LandingPage</div>
-  )
+  return <div>LandingPage</div>;
 }
 
-export default LandingPage
+export default LandingPage;
